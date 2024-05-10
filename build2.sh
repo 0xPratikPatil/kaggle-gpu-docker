7b6ae5754f538909c3377e0a037a343d909d5306
Pratik Patil <127599705+0xPratikPatil@users.noreply.github.com>
2024-05-10 12:06:14 +0530

    Create build2.sh

---
+++ build2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROJECT_PATH=/kaggle/working
+PASSWORD=<password of your jupyterlab>
+docker run --rm -it --gpus=all \
+    -v .:/kaggle \
+    -v $PROJECT_PATH/.cache/torch:/root/.cache/torch \
+    -v ./cache/huggingface:/root/.cache/huggingface \
+    -v $PROJECT_PATH/.kaggle:/root/.kaggle \
+    -p 9999:9999 \
+    kaggle_gpu /bin/bash -c "export PASSWORD=$PASSWORD; jupyter lab --no-browser --ip='0.0.0.0' --notebook-dir=/kaggle --NotebookApp.token='$PASSWORD' --NotebookApp.password='' --allow-root" 
